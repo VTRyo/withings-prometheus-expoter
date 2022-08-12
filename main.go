3b94d29adcb0921e81407e6e2cba5df56f147be0
VTRyo <vtryo.work@gmail.com>
2022-08-12 14:59:32 +0900

    localhost:8080/metrics \# HELP withings_fat_rate Fat Rate \# TYPE withings_fat_rate gauge withings_fat_rate{device_id="xxxxxxx"} 24.079 \# HELP withings_weight Weight value \# TYPE withings_weight gauge withings_weight{device_id="xxxx"} 70.60000000000001

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/VTRyo/withings-exporter/withings"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -12,25 +14,45 @@ import (
 var (
 	weightGage = promauto.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Namespace: "",
-			Name:      "",
-			Help:      "",
-		}, []string{""},
+			Namespace: "withings",
+			Name:      "weight",
+			Help:      "Weight value",
+		}, []string{"device_id"},
+	)
+
+	fatGage = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "withings",
+			Name:      "fat_rate",
+			Help:      "Fat Rate",
+		}, []string{"device_id"},
 	)
 )
 
 func main() {
-	prometheus.Register()
+	prometheus.Register(weightGage)
+	prometheus.Register(fatGage)
 
 	http.Handle("/metrics", promhttp.Handler())
 	go func() {
 		setValue()
 	}()
-	log.Fatal(http.ListenAndServe("8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func setValue() {
-	// 1日間隔で情報を取得する
-	// Labelを付与する
-	// 値をSetする
+	withings.SetToken()
+	weight := withings.GetWeight().Value
+	weightLabels := prometheus.Labels{
+		"device_id": withings.GetWeight().DeviceID,
+	}
+
+	fat := withings.GetFat().Value
+	fatLabels := prometheus.Labels{
+		"device_id": withings.GetFat().DeviceID,
+	}
+
+	weightGage.With(weightLabels).Set(weight)
+	fatGage.With(fatLabels).Set(fat)
+	time.Sleep(86400 * time.Second) // 1日間隔
 }
